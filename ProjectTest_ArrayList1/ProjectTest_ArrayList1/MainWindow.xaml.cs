ca965c87cf28a692aa659e7dc4e59e79bc2ca4cc
Newz CJ <newz.cj@gmail.com>
2019-11-25 22:44:54 +0700

    all

---
+++ ProjectTest_ArrayList1/ProjectTest_ArrayList1/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjectTest_ArrayList1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ArrayList arrayList;
+        public MainWindow()
+        {
+            InitializeComponent();
+            arrayList = new ArrayList();
+        }
+
+
+        private void ShowButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (string data in arrayList)
+            {
+                MessageBox.Show(data);
+            }
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            arrayList.Add(inputNum.Text);
+            inputNum.Text = "";
+        }
+
+        private void RemoveButton_Click(object sender, RoutedEventArgs e)
+        {
+            arrayList.Remove(inputNum.Text);
+            inputNum.Text = "";
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
